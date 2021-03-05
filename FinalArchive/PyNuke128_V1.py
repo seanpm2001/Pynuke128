11230a065a85f8bc241687565a2ea786f4500422
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-04 18:39:44 -0800

    PyNuke128_V1.py
    An archive of the first and final version of this mess.

---
+++ FinalArchive/PyNuke128_V1.py
@@ -0,0 +1,43 @@
+# Start of script
+# endless
+# print ("WARNING! THIS PROGRAM EXPONENTIATES 2 WITH THE 128 BIT INTEGER OF 340282366920938463463374607431768211455")
+# print ("WARNING! THIS PROGRAM EXPONENTIATES 340282366920938463463374607431768211455 WITH THE 128 BIT INTEGER OF 340282366920938463463374607431768211455")
+print ("If your computer isn't powerful enough, it will either crash, or use up heavy processing power")
+print ("Press [ENTER] key once to see the value, if it can even load the value. Press [ENTER] key again to start the bomb process")
+print ("Note: this program requires more than 16 Terabytes of RAM to run correctly")
+startUp = str(input("Continue? [ENTER]"))
+# x = 2**340282366920938463463374607431768211455
+x = 340282366920938463463374607431768211455**340282366920938463463374607431768211455
+y = x
+print ("Here is the value that is going to be spammed:")
+print (x)
+suicide = input("Are you really sure? This is pretty much suicide")
+for a in range(x)
+  print (x, y)
+else:
+  # Protogent joke ("salt" on the wound)
+  print ("OOPS YOUR SYSTEM CRASHED")
+  print ("But I had an anti-virus")
+  print ("Anti-virus is not enough! You need protogent!")
+  print ("The first anti-virus with deeta recovery")
+print ("Wow, is your system from the future? It has managed to survive being nuked duodecillion times over. Or did you just modify the script? (-_-)")
+# End of script
+'''
+WARNINF!
+THIS PROGRAM IS INTENDED FOR RESEARCH PURPOSES ONLY!
+THIS FORK BOMB CAN CAUSE SEVERE DAMAGE TO A SERVER, AND TAKE IT OFFLINE
+DO *NOT* USE THIS TO TAKE DOWN A SERVER LIKE GOOGLE, NO MATTER HOW MUCH YOU HATE THEM
+THIS WILL NOT ONLY TAKE THE SERVER DOWN, BUT POSSIBLY DAMAGE THE HARDWARE
+YOU WOULD NOT IMAGINE HOW MUCH DAMAGE THIS WOULD CAUSE
+SO *DON'T* *DO* *IT!*
+*THIS* *IS* *YOUR* *FINAL* *WARNING*
+NOTE: There are also some extra steps involved, including preventing the server from terminating the script, and other workarounds that aren't covered in this program.
+'''
+# this program was written by Sean Patrick Myrick
+# any damages caused with this program are on you. This was made for research and testing purposes
+# my computer can't even comprehend this program, it will just hang. Be very careful with this
+"""
+Written in: Python 3.7
+Lines of code: 42
+Size: 2,.321 bytes (2,.32 Kilobytes)
+"""
